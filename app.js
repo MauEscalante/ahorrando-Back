7c0e9d88c38ba5cc49576171eb4b1f63dcdae750
MauEscalante <Escalantemauricio2002@gmail.com>
2025-07-16 23:25:39 -0300

    .

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import cookieParser from 'cookie-parser';
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'], // Agrega aquí la URL de tu frontend
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000',"https://ahorrando-api.onrender.com"], // Agrega aquí la URL de tu frontend
     credentials: true, // Permite cookies
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
@@ -44,7 +44,7 @@ mongoose.connection.on('error', err => {
 });
 
 // Puerto del servidor
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || "https://ahorrando-api.onrender.com";
 
 // Iniciar servidor
 const server = app.listen(port, () => {
